b23e8be544cdb17c39b19ca747cd6699eefbca60
Drwalin <garekmarek@wp.pl>
2021-03-24 16:06:46 +0100

    TestsInitial tests

--- example/Example2.cpp
+++ example/Example2.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
-#include <VTable.hpp>
+#include <InterfaceGenerator.hpp>
+#include <Pointer.hpp>
 
 class MyClass {
 public:
@@ -10,49 +11,49 @@ public:
 	MyClass() {}
 	~MyClass() {}
 	
-	int Make1(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make1(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 342421;
 	}
-	int Make2(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make2(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 65346;
 	}
-	int Make3(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make3(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 543566;
 	}
-	int Make4(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make4(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 6537456;
 	}
-	int Make5(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make5(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 3412453;
 	}
-	int Make6(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make6(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return -34214;
 	}
-	int Make7(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make7(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 343254;
 	}
-	int Make8(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make8(std::string* data, float* a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 432143;
 	}
-	int Make9(std::string& data, float a) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make9(std::string* data, float a) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return 563545;
 	}
-	int Make(std::string& data) {
-		std::cout << "\n Running my class" << data << "  " << __PRETTY_FUNCTION__;
+	int Make(std::string* data) {
+		std::cout << "\n Running my class" << *data << "  " << __PRETTY_FUNCTION__;
 		return -31421;
 	}
-	int Run(std::string& mleko) {
-		std::cout << "\n Running my class" << mleko << "  " << __PRETTY_FUNCTION__;
-		mleko = "Zmieniononnnnneee 123";
+	int Run(std::string* mleko) {
+		std::cout << "\n Running my class" << *mleko << "  " << __PRETTY_FUNCTION__;
+		*mleko = "Zmieniononnnnneee 123";
 		return 789012;
 	}
 private:
@@ -75,6 +76,15 @@ GENERATE_CALLABLE_VTABLE_HEADER(MyClass,
 		_(Make9)
 		);
 
+#define MAG Ciak
+
+#define APPEND(A, B) REEVALUATE(A, B)
+#define REEVALUATE(S, B) S##B
+int APPEND(MAG,Ups)() {
+	printf("\n elo");
+	return 123;
+}
+
 #include <cstdio>
 int main() {
 	std::cout<<"\n 1";
@@ -86,11 +96,13 @@ int main() {
 	std::cout << "\n vtable:       " << &MyClass_vtable;
 	std::cout << "\n self->vtable: " << p.self->self;
 	
+	
+	
 	std::string str = "Z majna";
 	p.Call<int>(1, &str);
 	std::cout<<"\n 4 - str: " << str;
 	
-	
+	CiakUps();
 	
 	
 	return 0;
---
+++ src/TypeName.hpp
@@ -0,0 +1,81 @@
+/*
+ *  This file is part of ModulHotReload. Please see README for details.
+ *  Copyright (C) 2021 Marek Zalewski aka Drwalin
+ *
+ *  ICon3 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICon3 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TYPE_NAME_HPP
+#define TYPE_NAME_HPP
+
+#include <cstring>
+#include <cstdio>
+#include <string_view>
+
+// Based on https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c
+template<typename T>
+constexpr auto type_name() noexcept {
+#ifdef __clang__
+	const char* name = __PRETTY_FUNCTION__;
+	const size_t length = sizeof(__PRETTY_FUNCTION__)-1;
+	const size_t prefix = sizeof("constexpr auto type_name() [T = ")-1;
+	const size_t suffix = sizeof("]")-1;
+#elif defined(__GNUC__)
+	const char* name = __PRETTY_FUNCTION__;
+	const size_t length = sizeof(__PRETTY_FUNCTION__)-1;
+	const size_t prefix = sizeof("constexpr auto type_name() [with T = ")-1;
+	const size_t suffix = sizeof("]")-1;
+#elif defined(_MSC_VER)
+	const char* name = __FUNCSIG__;
+	const size_t length = sizeof(__FUNCSIG__)-1;
+	const size_t prefix = sizeof("constexpr auto __cdecl type_name<")-1;
+	const size_t suffix = sizeof(">(void) noexcept")-1;
+#else
+	static_assert(false, "Unknown compiler. Please specify your own code for type_name, prfix and suffix");
+#endif
+	const size_t new_length = length-suffix-prefix;
+	return (char*const)({
+		char* str = (char*)malloc(new_length+1);
+		memcpy(str, name+prefix, new_length);
+		str[new_length] = 0;
+		str;
+	});
+}
+
+
+template<typename T>
+constexpr auto TypeName() noexcept {
+#ifdef __clang__
+	constexpr const char* name = __PRETTY_FUNCTION__;
+	constexpr const size_t length = sizeof(__PRETTY_FUNCTION__)-1;
+	constexpr const size_t prefix = sizeof("constexpr auto type_name() [T = ")-1;
+	constexpr const size_t suffix = sizeof("]")-1;
+#elif defined(__GNUC__)
+	constexpr const char* name = __PRETTY_FUNCTION__;
+	constexpr const size_t length = sizeof(__PRETTY_FUNCTION__)-1;
+	constexpr const size_t prefix = sizeof("constexpr auto type_name() [with T = ")-1;
+	constexpr const size_t suffix = sizeof("]")-1;
+#elif defined(_MSC_VER)
+	constexpr const char* name = __FUNCSIG__;
+	constexpr const size_t length = sizeof(__FUNCSIG__)-1;
+	constexpr const size_t prefix = sizeof("constexpr auto __cdecl type_name<")-1;
+	constexpr const size_t suffix = sizeof(">(void) noexcept")-1;
+#else
+	static_assert(false, "Unknown compiler. Please specify your own code for type_name, prfix and suffix");
+#endif
+	constexpr const size_t new_length = length-suffix-prefix;
+	return std::string(name+prefix-1, new_length+1);
+}
+
+#endif
